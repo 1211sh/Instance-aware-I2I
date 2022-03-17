984f007087d13965dea363e27819552d6944b75f
Soohyun <33894603+1211sh@users.noreply.github.com>
2022-03-17 22:40:25 +0900

    initial code

---
+++ options/test_options.py
@@ -0,0 +1,23 @@
+from .base_options import BaseOptions
+
+
+class TestOptions(BaseOptions):
+    """This class includes test options.
+
+    It also includes shared options defined in BaseOptions.
+    """
+
+    def initialize(self, parser):
+        parser = BaseOptions.initialize(self, parser)  # define shared options
+        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+        # Dropout and Batchnorm has different behavioir during training and test.
+        parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
+        parser.add_argument('--num_test', type=int, default=500, help='how many test images to run')
+        parser.add_argument('--num', type=int, default=5, help='number of outputs per image') # 0828 jh
+        parser.add_argument('--a2b', type=int, default=1, help='translation direction, 1 for a2b, 0 for b2a') #0828
+        
+        # To avoid cropping, the load_size should be the same as crop_size
+        parser.set_defaults(load_size=parser.get_default('crop_size'))
+        self.isTrain = False
+        return parser
