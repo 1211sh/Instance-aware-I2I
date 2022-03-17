f5d7024e2cd8ab68075b9bc65a23c0d83f4bd3d2
Soohyun <33894603+1211sh@users.noreply.github.com>
2022-03-17 22:37:19 +0900

    initial code

---
+++ models/positional_encoding.py
@@ -0,0 +1,24 @@
+import math
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [batch_size, seq_len, embedding_dim]
+        """
+        return self.pe.permute(1, 0, 2)
